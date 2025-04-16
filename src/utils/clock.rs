b62f8a06e9ed612ad8961aad42b56e1c520cb795
andreimontchik <andrei@mrgn.group>
2025-04-16 09:11:11 -0500

    CLock logging

--- src/utils/clock.rs
+++ src/utils/clock.rs
@@ -5,6 +5,7 @@ use solana_client::rpc_client::RpcClient;
 use solana_sdk::clock::Clock;
 use solana_sdk::sysvar::{self};
 use std::sync::{Arc, Mutex};
+use std::thread;
 use std::time::{Duration, Instant};
 
 pub struct CachedClock {
@@ -29,14 +30,23 @@ impl CachedClock {
 
         // Check if the cache is stale
         if Instant::now() - *last_updated >= self.cache_duration {
-            debug!("Updating clock cache...");
+            debug!(
+                "Thread {:?}: Updating clock cache...",
+                thread::current().id()
+            );
             let clock_account = rpc_client.get_account(&sysvar::clock::id())?;
             *clock = deserialize(&clock_account.data)?;
             *last_updated = Instant::now();
-            debug!("Completed the clock cache update.");
+            debug!(
+                "Thread {:?}: Completed the clock cache update.",
+                thread::current().id()
+            );
         }
 
-        debug!("Completed the Clock cache evaluation.");
+        debug!(
+            "Thread {:?}: Completed the Clock cache evaluation.",
+            thread::current().id()
+        );
         Ok(clock.clone())
     }
 }
