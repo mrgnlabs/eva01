7fee6c4140082730ab38de8b7b91efd86228c327
andreimontchik <andrei@mrgn.group>
2025-05-01 14:33:55 -0500

    Added unit tests to the Oracles cache.

--- src/cache/oracles.rs
+++ src/cache/oracles.rs
@@ -141,3 +141,109 @@ impl<T: OracleWrapperTrait + Clone> OraclesCache<T> {
         Ok(*bank_address)
     }
 }
+#[cfg(test)]
+mod tests {
+    use crate::wrappers::oracle::test_utils::TestOracleWrapper;
+
+    use super::*;
+
+    #[test]
+    fn test_try_insert() {
+        let mut cache = OraclesCache::<TestOracleWrapper>::new();
+        let oracle_account = Account::default();
+        let oracle_wrapper = TestOracleWrapper::test_sol();
+        let oracle_address = oracle_wrapper.address;
+        let bank_address = Pubkey::new_unique();
+
+        assert!(cache
+            .try_insert(oracle_account.clone(), oracle_wrapper.clone(), bank_address)
+            .is_ok());
+        assert_eq!(cache.accounts.get(&oracle_address), Some(&oracle_account));
+        assert!(cache
+            .account_wrappers
+            .read()
+            .unwrap()
+            .contains_key(&oracle_address));
+        assert_eq!(
+            cache.oracle_to_bank.get(&oracle_address),
+            Some(&bank_address)
+        );
+        assert_eq!(
+            cache.bank_to_oracle.get(&bank_address),
+            Some(&oracle_address)
+        );
+    }
+
+    #[test]
+    fn test_get_accounts() {
+        let mut cache = OraclesCache::<TestOracleWrapper>::new();
+        let address1 = Pubkey::new_unique();
+        let account1 = Account::default();
+        cache.accounts.insert(address1, account1.clone());
+        let address2 = Pubkey::new_unique();
+        let account2 = Account::default();
+        cache.accounts.insert(address2, account2.clone());
+
+        let accounts = cache.get_accounts(&[address1, address2]);
+        assert_eq!(accounts.len(), 2);
+        assert!(accounts.contains(&(address1, account1)));
+        assert!(accounts.contains(&(address2, account2)));
+    }
+
+    #[test]
+    fn test_get_addresses() {
+        let cache = OraclesCache::<TestOracleWrapper>::new();
+        let wrapper1 = TestOracleWrapper::test_sol();
+        let address1 = wrapper1.address;
+        let wrapper2 = TestOracleWrapper::test_usdc();
+        let address2 = wrapper2.address;
+
+        cache
+            .account_wrappers
+            .write()
+            .unwrap()
+            .insert(address1, wrapper1);
+        cache
+            .account_wrappers
+            .write()
+            .unwrap()
+            .insert(address2, wrapper2);
+
+        let addresses = cache.get_addresses();
+        assert_eq!(addresses.len(), 2);
+        assert!(addresses.contains(&address1));
+        assert!(addresses.contains(&address2));
+    }
+
+    #[test]
+    fn test_get_wrapper_from_bank() {
+        let mut cache = OraclesCache::<TestOracleWrapper>::new();
+        let bank_address = Pubkey::new_unique();
+        let oracle_address = Pubkey::new_unique();
+        let mut wrapper = TestOracleWrapper::test_sol();
+        wrapper.address = oracle_address.clone();
+
+        cache.bank_to_oracle.insert(bank_address, oracle_address);
+        cache
+            .account_wrappers
+            .write()
+            .unwrap()
+            .insert(oracle_address, wrapper.clone());
+
+        let retrieved_wrapper = cache.get_wrapper_from_bank(&bank_address);
+        assert!(retrieved_wrapper.is_some());
+        assert_eq!(retrieved_wrapper.unwrap().get_address(), oracle_address);
+    }
+
+    #[test]
+    fn test_try_get_bank_from_oracle() {
+        let mut cache = OraclesCache::<TestOracleWrapper>::new();
+        let bank_address = Pubkey::new_unique();
+        let oracle_address = Pubkey::new_unique();
+
+        cache.oracle_to_bank.insert(oracle_address, bank_address);
+
+        let retrieved_bank = cache.try_get_bank_from_oracle(&oracle_address).unwrap();
+        assert_eq!(retrieved_bank, bank_address);
+    }
+}
